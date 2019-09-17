dd96e71e8f4623dd9a6945214edfff0248d69a26
Mariah Mensah <mariahm207@gmail.com>
2019-09-17 17:08:35 -0500

    Updates

---
+++ precache-manifest.f8a59e1665d98157d14d0fd274ee55d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a75d4052f198f1de7478478023f3a0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "544f912480503353c1de",
+    "url": "/portfolio/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "e0867860c73314e19b1b",
+    "url": "/portfolio/static/css/main.a7458c99.chunk.css"
+  },
+  {
+    "revision": "544f912480503353c1de",
+    "url": "/portfolio/static/js/2.ae3ade35.chunk.js"
+  },
+  {
+    "revision": "e0867860c73314e19b1b",
+    "url": "/portfolio/static/js/main.a719939a.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "6c08b192fdbd5f0d2630afd72c0c47fd",
+    "url": "/portfolio/static/media/calc.6c08b192.png"
+  },
+  {
+    "revision": "2c8c69fc4ad33f9c88fcba2024d18f0a",
+    "url": "/portfolio/static/media/gallery.2c8c69fc.png"
+  },
+  {
+    "revision": "64a6911c7c8a8c1c32f0c9a8b198711b",
+    "url": "/portfolio/static/media/water.64a6911c.png"
+  }
+]);
